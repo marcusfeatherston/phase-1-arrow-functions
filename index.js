7c0bb41ade43b24e783576b6fbcbdca3eef33ba4
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-11 11:11:14 -0700

    created math arrow functions

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (number1, number2) => {
+  return number1 / number2;
+};
+
+const square = (number) => {
+  return number * number;
+};
+
+const add = (number1, number2) => {
+  const answer = number1 + number2;
+  return answer;
+}
+
+
+
+
+
